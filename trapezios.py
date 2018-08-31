a86cc012e892a077e8e28aac8ccfec5062843b14
hugoadriao <hugovictorsilva94@gmail.com>
2018-08-31 15:17:06 -0300

    Adição de comentários no código

--- trapezios.py
+++ trapezios.py
@@ -1,3 +1,4 @@
+#Criacao da funcao que funciona como f(x)
 def func(x):
     from math import exp, sqrt, sin
     #f = 1/x
@@ -7,15 +8,21 @@ def func(x):
     #f = sin(x)
     f = exp(x)
     return f
+#Criacao do algoritmo
 def trapezio(a, b, n):
     h = (b-a)/n
     x = []
+    #Definicao do primeiro ponto depois de a
     pm1 = a+h
+    #Insere o primeiro ponto na lista geral
     x.append(pm1)
+    #Insere o primeiro valor no somatorio
     soma_x = func(pm1)
+    #Insere os valores na lista e calcula o somatorio dos valores na funcao
     for i in range(1, n-1, 1):
         x.append(x[i-1]+h)
         soma_x += func(x[i])
+    #Formula
     result = (h/2)*(func(a)+(2*(soma_x))+func(b))
     #print('lista =', x)
     print('resultado =', result)
